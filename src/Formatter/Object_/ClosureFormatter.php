fd64e3a7c88568e8e4e5a5d1c70d276417955acd
kafoso <soefritz@gmail.com>
2021-09-09 12:01:12 +0200

    Impl. ClosureFormatter

---
+++ src/Formatter/Object_/ClosureFormatter.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eboreum\Caster\Formatter\Object_;
+
+use Eboreum\Caster\Abstraction\Formatter\AbstractObjectFormatter;
+use Eboreum\Caster\Contract\CasterInterface;
+use Eboreum\Caster\Caster;
+
+/**
+ * Formatter for \Closure.
+ */
+class ClosureFormatter extends AbstractObjectFormatter
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function format(CasterInterface $caster, object $object): ?string
+    {
+        if (false === $this->isHandling($object)) {
+            return null; // Pass on
+        }
+
+        $arguments = [];
+
+        $reflectionFunction = new \ReflectionFunction($object);
+
+        foreach ($reflectionFunction->getParameters() as $reflectionParameter) {
+            $argument = "\${$reflectionParameter->getName()}";
+
+            if ($reflectionParameter->isPassedByReference()) {
+                $argument = "&" . $argument;
+            }
+
+            if ($reflectionParameter->isVariadic()) {
+                $argument = "..." . $argument;
+            }
+
+            if ($reflectionParameter->hasType()) {
+                $reflectionType = $reflectionParameter->getType();
+                $typeText = $reflectionType->getName();
+
+                if (class_exists($typeText)) {
+                    $typeText = "\\{$typeText}";
+                }
+
+                if ($reflectionParameter->allowsNull()) {
+                    $typeText = "?" . $typeText;
+                }
+
+                if ($reflectionParameter->isDefaultValueAvailable()) {
+                    if ($reflectionParameter->isDefaultValueConstant()) {
+                        $constantName = $reflectionParameter->getDefaultValueConstantName();
+
+                        if (preg_match('/\\\\/', $constantName)) {
+                            $constantName = "\\{$constantName}";
+                        }
+
+                        $argument .= " = " . $constantName;
+                    } else {
+                        $argument .= " = " . $caster->cast($reflectionParameter->getDefaultValue());
+                    }
+                }
+
+                $argument = "{$typeText} {$argument}";
+            }
+
+            $arguments[] = $argument;
+        }
+
+        return sprintf(
+            "%s(%s)",
+            Caster::makeNormalizedClassName(new \ReflectionObject($object)),
+            implode(", ", $arguments)
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isHandling(object $object): bool
+    {
+        return $object instanceof \Closure;
+    }
+}
---
+++ tests/tests/Test/Unit/Formatter/Object_/ClosureFormatterTest.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Eboreum\Caster\Formatter\Object_;
+
+use Eboreum\Caster\Caster;
+use Eboreum\Caster\Collection\Formatter\ObjectFormatterCollection;
+use Eboreum\Caster\Formatter\Object_\ClosureFormatter;
+use PHPUnit\Framework\TestCase;
+
+class ClosureFormatterTest extends TestCase
+{
+    const A_CONSTANT = "foo";
+
+    public function testFormatReturnsNullWhenObjectIsNotQualified(): void
+    {
+        $caster = Caster::create();
+        $closureFormatter = new ClosureFormatter;
+        $object = new \stdClass;
+
+        $this->assertFalse($closureFormatter->isHandling($object));
+        $this->assertNull($closureFormatter->format($caster, $object));
+    }
+
+    /**
+     * @dataProvider dataProvider_testFormatWorks
+     */
+    public function testFormatWorks(
+        string $message,
+        \Closure $closure,
+        string $expectedArguments
+    ): void
+    {
+        $caster = Caster::create();
+        $closureFormatter = new ClosureFormatter;
+
+        $this->assertTrue($closureFormatter->isHandling($closure), $message);
+        $this->assertMatchesRegularExpression(
+            sprintf(
+                implode("", [
+                    '/',
+                    '^',
+                    '\\\\Closure\(%s\)',
+                    '$',
+                    '/',
+                ]),
+                preg_quote($expectedArguments, "/"),
+            ),
+            $closureFormatter->format($caster, $closure),
+            $message,
+        );
+    }
+
+    /**
+     * @return array<int, array{string, \Closure, string}>
+     */
+    public function dataProvider_testFormatWorks(): array
+    {
+        return [
+            [
+                "\Closure with no arguments.",
+                function(){},
+                "",
+            ],
+            [
+                "\Closure with 1 argument. No default value.",
+                function(int $a){},
+                "int \$a",
+            ],
+            [
+                "\Closure with 1 argument. With default value.",
+                function(int $a = 42){},
+                "int \$a = 42",
+            ],
+            [
+                "\Closure with 1 argument. With default value being a global constant.",
+                function(int $a = \PHP_INT_MAX){},
+                "int \$a = PHP_INT_MAX",
+            ],
+            [
+                "\Closure with 1 argument. With default value being a constant with a `self` reference.",
+                function(int $a = self::A_CONSTANT){},
+                "int \$a = self::A_CONSTANT",
+            ],
+            [
+                "\Closure with 1 argument. With default value being a constant with a class name reference.",
+                function(int $a = ClosureFormatterTest::A_CONSTANT){},
+                sprintf(
+                    "int \$a = \\%s::A_CONSTANT",
+                    ClosureFormatterTest::class,
+                ),
+            ],
+            [
+                "\Closure with 3 arguments. No default values.",
+                function(int $a, string $b, bool $c){},
+                "int \$a, string \$b, bool \$c",
+            ],
+            [
+                "\Closure with 3 arguments. With 1 default value.",
+                function(int $a = 42, string $b, bool $c){},
+                "int \$a = 42, string \$b, bool \$c",
+            ],
+            [
+                "\Closure with 3 arguments. With 2 default values.",
+                function(int $a = 42, string $b = "foo", bool $c){},
+                "int \$a = 42, string \$b = \"foo\", bool \$c",
+            ],
+            [
+                "\Closure with 3 arguments. With 3 default values.",
+                function(int $a = 42, string $b = "foo", bool $c = true){},
+                "int \$a = 42, string \$b = \"foo\", bool \$c = true",
+            ],
+            [
+                "\Closure with 1 typed variadic argument.",
+                function(int ...$a){},
+                "int ...\$a",
+            ],
+            [
+                "\Closure with 1 typed variadic argument being nullable.",
+                function(?int ...$a){},
+                "?int ...\$a",
+            ],
+            [
+                "\Closure with 1 typed argument passed by reference.",
+                function(int &$a){},
+                "int &\$a",
+            ],
+            [
+                "\Closure with 1 typed argument passed by reference being nullable.",
+                function(?int &$a){},
+                "?int &\$a",
+            ],
+            [
+                "The big one.",
+                function($a, &$b, int $c, bool $d, \stdClass $e, array $f = ["lala"], ?string ...$z){},
+                "\$a, &\$b, int \$c, bool \$d, \\stdClass \$e, array \$f = [0 => \"lala\"], ?string ...\$z",
+            ],
+        ];
+    }
+}
