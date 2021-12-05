20428d85ea2eb593fd1d0f9d363eefa70633a2fe
kafoso <soefritz@gmail.com>
2021-12-05 20:32:39 +0100

    Implemented DebugInfoFormatter

---
+++ src/Formatter/Object_/DebugInfoFormatter.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eboreum\Caster\Formatter\Object_;
+
+use Eboreum\Caster\Abstraction\Formatter\AbstractObjectFormatter;
+use Eboreum\Caster\Caster;
+use Eboreum\Caster\Contract\CasterInterface;
+
+/**
+ * Handles classes, which implement the magic method `__debugInfo`.
+ *
+ * @see https://www.php.net/manual/en/language.oop5.magic.php#object.debuginfo
+ */
+class DebugInfoFormatter extends AbstractObjectFormatter
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function format(CasterInterface $caster, object $object): ?string
+    {
+        if (false === $this->isHandling($object)) {
+            return null; // Pass on
+        }
+
+        assert(method_exists($object, '__debugInfo')); // Make phpstan happy
+
+        return sprintf(
+            '%s (%s)',
+            Caster::makeNormalizedClassName(new \ReflectionObject($object)),
+            $caster->cast($object->__debugInfo()),
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isHandling(object $object): bool
+    {
+        return method_exists($object, '__debugInfo');
+    }
+}
---
+++ tests/tests/Test/Unit/Formatter/Object_/DebugInfoFormatterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Eboreum\Caster\Formatter\Object_;
+
+use Eboreum\Caster\Caster;
+use Eboreum\Caster\Formatter\Object_\DebugInfoFormatter;
+use PHPUnit\Framework\TestCase;
+
+class DebugInfoFormatterTest extends TestCase
+{
+    public function testFormatReturnsNullWhenObjectIsNotQualified(): void
+    {
+        $caster = Caster::create();
+        $debugInfoFormatter = new DebugInfoFormatter();
+
+        $object = new class {};
+
+        $this->assertFalse($debugInfoFormatter->isHandling($object));
+        $this->assertNull($debugInfoFormatter->format($caster, $object));
+    }
+
+    public function testFormatWorks(): void
+    {
+        $caster = Caster::create();
+        $debugInfoFormatter = new DebugInfoFormatter();
+
+        $object = new class
+        {
+            /**
+             * @return array<string, string>
+             */
+            public function __debugInfo(): array
+            {
+                return ['foo' => 'bar'];
+            }
+        };
+
+        $this->assertTrue($debugInfoFormatter->isHandling($object));
+        $formatted = $debugInfoFormatter->format($caster, $object);
+        $this->assertIsString($formatted);
+        assert(is_string($formatted)); // Make phpstan happy
+        $this->assertMatchesRegularExpression(
+            sprintf(
+                implode('', [
+                    '/',
+                    '^',
+                    'class@anonymous\/in\/.+\/%s\:\d+ \(\[',
+                        '"foo" \=\> "bar"',
+                    '\]\)',
+                    '$',
+                    '/',
+                ]),
+                preg_quote(basename(__FILE__), '/'),
+            ),
+            $formatted,
+        );
+    }
+}
